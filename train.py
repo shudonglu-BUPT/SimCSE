fc67fee8f1d7ea7fa8c7ffa3684b8dc5707e9673
shudonglu-BUPT <601387763@qq.com>
2023-08-21 16:01:14 +0800

    Update train.py

--- train.py
+++ train.py
@@ -349,6 +349,10 @@ def main():
             "You can do it from another script, save it, and load it from here, using --tokenizer_name."
         )
 
+    #extent vocabulary
+    ts_token, te_token = ["[T_S]", "[T_E]"]
+    tokenizer.add_tokens([ts_token, te_token])
+
     def word2wordpiece_offset(example):
         w2wp = np.array([0] + example["token_lens"]).cumsum()
         entity_id2entity = {}
@@ -454,13 +458,10 @@ def main():
         # token_type_ids
         # attention_mask
         # labels
-    
-        example["input_ids_2"] = p_example["input_ids"]
-        example["token_type_ids_2"] = p_example["token_type_ids"]
-        example["attention_mask_2"] = p_example["attention_mask"]
-        example["input_ids_3"] = n_example["input_ids"]
-        example["token_type_ids_3"] = n_example["token_type_ids"]
-        example["attention_mask_3"] = n_example["attention_mask"]
+        example["input_ids"] = [example["input_ids"], p_example["input_ids"], n_example["input_ids"]]
+        example["token_type_ids"] = [example["token_type_ids"], p_example["token_type_ids"], n_example["token_type_ids"]]
+        example["attention_mask"] = [example["attention_mask"], p_example["attention_mask"], n_example["attention_mask"]]
+        
         return example
 
     if model_args.model_name_or_path:
@@ -496,76 +497,77 @@ def main():
 
     model.resize_token_embeddings(len(tokenizer))
 
-    # Prepare features
-    column_names = datasets["train"].column_names
-    sent2_cname = None
-    if len(column_names) == 2:
-        # Pair datasets
-        sent0_cname = column_names[0]
-        sent1_cname = column_names[1]
-    elif len(column_names) == 3:
-        # Pair datasets with hard negatives
-        sent0_cname = column_names[0]
-        sent1_cname = column_names[1]
-        sent2_cname = column_names[2]
-    elif len(column_names) == 1:
-        # Unsupervised datasets
-        sent0_cname = column_names[0]
-        sent1_cname = column_names[0]
-    else:
-        raise NotImplementedError
+    # # Prepare features
+    # column_names = datasets["train"].column_names
+    # sent2_cname = None
+    # if len(column_names) == 2:
+    #     # Pair datasets
+    #     sent0_cname = column_names[0]
+    #     sent1_cname = column_names[1]
+    # elif len(column_names) == 3:
+    #     # Pair datasets with hard negatives
+    #     sent0_cname = column_names[0]
+    #     sent1_cname = column_names[1]
+    #     sent2_cname = column_names[2]
+    # elif len(column_names) == 1:
+    #     # Unsupervised datasets
+    #     sent0_cname = column_names[0]
+    #     sent1_cname = column_names[0]
+    # else:
+    #     raise NotImplementedError
 
-    def prepare_features(examples):
-        # padding = longest (default)
-        #   If no sentence in the batch exceed the max length, then use
-        #   the max sentence length in the batch, otherwise use the 
-        #   max sentence length in the argument and truncate those that
-        #   exceed the max length.
-        # padding = max_length (when pad_to_max_length, for pressure test)
-        #   All sentences are padded/truncated to data_args.max_seq_length.
-        total = len(examples[sent0_cname])
+    # def prepare_features(examples):
+    #     # padding = longest (default)
+    #     #   If no sentence in the batch exceed the max length, then use
+    #     #   the max sentence length in the batch, otherwise use the 
+    #     #   max sentence length in the argument and truncate those that
+    #     #   exceed the max length.
+    #     # padding = max_length (when pad_to_max_length, for pressure test)
+    #     #   All sentences are padded/truncated to data_args.max_seq_length.
+    #     total = len(examples[sent0_cname])
 
-        # Avoid "None" fields 
-        for idx in range(total):
-            if examples[sent0_cname][idx] is None:
-                examples[sent0_cname][idx] = " "
-            if examples[sent1_cname][idx] is None:
-                examples[sent1_cname][idx] = " "
+    #     # Avoid "None" fields 
+    #     for idx in range(total):
+    #         if examples[sent0_cname][idx] is None:
+    #             examples[sent0_cname][idx] = " "
+    #         if examples[sent1_cname][idx] is None:
+    #             examples[sent1_cname][idx] = " "
         
-        sentences = examples[sent0_cname] + examples[sent1_cname]
+    #     sentences = examples[sent0_cname] + examples[sent1_cname]
 
-        # If hard negative exists
-        if sent2_cname is not None:
-            for idx in range(total):
-                if examples[sent2_cname][idx] is None:
-                    examples[sent2_cname][idx] = " "
-            sentences += examples[sent2_cname]
+    #     # If hard negative exists
+    #     if sent2_cname is not None:
+    #         for idx in range(total):
+    #             if examples[sent2_cname][idx] is None:
+    #                 examples[sent2_cname][idx] = " "
+    #         sentences += examples[sent2_cname]
 
-        sent_features = tokenizer(
-            sentences,
-            max_length=data_args.max_seq_length,
-            truncation=True,
-            padding="max_length" if data_args.pad_to_max_length else False,
-        )
+    #     sent_features = tokenizer(
+    #         sentences,
+    #         max_length=data_args.max_seq_length,
+    #         truncation=True,
+    #         padding="max_length" if data_args.pad_to_max_length else False,
+    #     )
 
-        features = {}
-        if sent2_cname is not None:
-            for key in sent_features:
-                features[key] = [[sent_features[key][i], sent_features[key][i+total], sent_features[key][i+total*2]] for i in range(total)]
-        else:
-            for key in sent_features:
-                features[key] = [[sent_features[key][i], sent_features[key][i+total]] for i in range(total)]
+    #     features = {}
+    #     if sent2_cname is not None:
+    #         for key in sent_features:
+    #             features[key] = [[sent_features[key][i], sent_features[key][i+total], sent_features[key][i+total*2]] for i in range(total)]
+    #     else:
+    #         for key in sent_features:
+    #             features[key] = [[sent_features[key][i], sent_features[key][i+total]] for i in range(total)]
             
-        return features
+    #     return features
 
     if training_args.do_train:
-        train_dataset = datasets["train"].map(
-            prepare_features,
-            batched=True,
-            num_proc=data_args.preprocessing_num_workers,
-            remove_columns=column_names,
-            load_from_cache_file=not data_args.overwrite_cache,
-        )
+        # train_dataset = datasets["train"].map(
+        #     prepare_features,
+        #     batched=True,
+        #     num_proc=data_args.preprocessing_num_workers,
+        #     remove_columns=column_names,
+        #     load_from_cache_file=not data_args.overwrite_cache,
+        # )
+        train_dataset = datasets["train"]
 
     # Data collator
     @dataclass
